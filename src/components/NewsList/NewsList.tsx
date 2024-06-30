525616fff5e4864bc7d42e7c7ffdde42f43e0c55
Mytth <Mytth75@gmail.com>
2024-06-30 15:24:54 +0700

    init

---
+++ src/components/NewsList/NewsList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function NewsList({}: Props) {
+  return (
+    <div>NewsList</div>
+  )
+}
+
+export default NewsList
---
+++ src/components/NewsMain/NewsMain.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function NewsMain({}: Props) {
+  return (
+    <div>NewsMain</div>
+  )
+}
+
+export default NewsMain
